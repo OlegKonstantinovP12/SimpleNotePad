b908bd2ed963acba57ccea3c2c10f53460023397
Oleg Konstantinov <oleg.boss@gmail.com>
2026-03-29 17:42:01 +0200

    Initial Commit

---
+++ SimpleNotePad/SimpleNotePadApp.swift
@@ -0,0 +1,15 @@
+//
+//
+//  Created by Oleg Konstantinov.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
